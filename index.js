6d624e1e26954715425125aebff2cd6b1d8aec69
Mackleode Ben-Boulo <mackleodebenboulo@gmail.com>
2024-10-28 12:25:13 +0000

    completed project

--- index.js
+++ index.js
@@ -21,8 +21,21 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+  res.send("Worked")
+  console.log(req.headers)
 });
+
+app.get('/api/whoami', (req, res) => {
+  ip_addr = req.headers['x-forwarded-for']
+  pref_lang = req.headers['accept-language']
+  software = req.headers['user-agent']
+
+  res.json({
+    ipaddress: ip_addr,
+    language: pref_lang,
+    software: software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
